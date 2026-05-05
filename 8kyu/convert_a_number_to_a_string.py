a4d7cd15dc025151eeb4f3b3d46a1030c7b702d1
Tusharsharma2852 <87015188+Tusharsharma2852@users.noreply.github.com>
2026-05-05 14:47:17 +0530

    Add function to convert number to string

---
+++ 8kyu/convert_a_number_to_a_string.py
@@ -0,0 +1,4 @@
+#I just write down this code to scratch my foundation of Python , I know it is basic but important too.
+#Link : https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+def number_to_string(num):
+    return f"{num}"

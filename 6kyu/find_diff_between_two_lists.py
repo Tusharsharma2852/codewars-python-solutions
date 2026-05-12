4d11e8a10fd57a0af857a4d58cb6415e11a1f43c
Tusharsharma2852 <87015188+Tusharsharma2852@users.noreply.github.com>
2026-05-12 12:16:02 +0530

    Add function to find difference between two lists
    Implement function to find difference between two lists by removing common elements.

---
+++ 6kyu/find_diff_between_two_lists.py
@@ -0,0 +1,10 @@
+#Description : In this cpde i have two given list so my work is remove that element which is common in both list
+# Link : https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+def diff(a,b):
+  c = a+b
+  for i in c:
+    if i in a and i in b:
+      a.remove[i]
+  print(a)
+
+diff([1, 2, 2, 2, 3],[2])
